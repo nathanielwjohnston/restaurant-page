34a8906354459ccc45d049f7098ddbf2fbbcd7fd
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-09-22 20:39:08 +0100

    Add basic header styling

--- src/index.js
+++ src/index.js
@@ -10,11 +10,22 @@ const homeButton = document.querySelector("#home-tab-button");
 const menuButton = document.querySelector("#menu-tab-button");
 const contactButton = document.querySelector("#contact-tab-button");
 
+function resetCurrentTab(newTabButton) {
+  const currentTabButton = document.querySelector(".current-tab");
+
+  if (newTabButton !== currentTabButton) {
+    currentTabButton.classList.remove("current-tab");
+    newTabButton.classList.add("current-tab");
+  }
+}
+
 homeButton.addEventListener("click", () => {
   const pageContent = document.querySelector("#content");
   pageContent.replaceChildren();
 
   loadHomePage();
+
+  resetCurrentTab(document.querySelector("#home-tab-button"));
 })
 
 menuButton.addEventListener("click", () => {
@@ -22,6 +33,8 @@ menuButton.addEventListener("click", () => {
   pageContent.replaceChildren();
 
   loadMenu(menuItems);
+
+  resetCurrentTab(document.querySelector("#menu-tab-button"));
 })
 
 contactButton.addEventListener("click", () => {
@@ -30,5 +43,7 @@ contactButton.addEventListener("click", () => {
     pageContent.replaceChildren();
 
     loadContactPage();
+
+    resetCurrentTab(document.querySelector("#contact-tab-button"));
   }
 })
